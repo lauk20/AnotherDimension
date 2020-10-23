a5c9e828450a5bc43019ab25255403f2d36ba800
lauk20 <klau20@stuy.edu>
2020-10-23 12:19:43 -0400

    Added test in Tester and method in ArrayOps for largest()

--- ArrayOps.java
+++ ArrayOps.java
@@ -10,4 +10,16 @@ public class ArrayOps{
     return sum;
   }
 
+  public static int largest(int[] arr){
+    int largest = arr[0];
+
+    for (int i = 1; i < arr.length; i++){
+      if (arr[i] > largest){
+        largest = arr[i];
+      }
+    }
+
+    return largest;
+  }
+
 }
--- Tester.java
+++ Tester.java
@@ -2,5 +2,7 @@ public class Tester{
   public static void main(String[] args){
     int[] SumTest = {1, 2 , 3 , 4 , 5};
     System.out.println(ArrayOps.sum(SumTest));
+    System.out.println(ArrayOps.sum(new int[]{}));
+    System.out.println(ArrayOps.largest(SumTest));
   }
 }
